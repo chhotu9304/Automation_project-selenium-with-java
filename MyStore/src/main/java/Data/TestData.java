a9945a86a009b9243384c9fde5a42a031a8420c4
chhotu kumar <krchhotu9304@gmail.com>
2025-07-21 02:48:48 +0530

    new data

--- MyStore/src/main/java/Data/TestData.java
+++ MyStore/src/main/java/Data/TestData.java
@@ -6,7 +6,7 @@ public class TestData {
     public static final String ADMIN_PASSWORD = "*****";
     public static final String SELLER_EMAIL = "asd@gmail.com";
     public static final String PRODUCT_NAME = "akar";
-    public static final String PRODUCT_PRICE = "100";
+    public static final String PRODUCT_PRICE = "200";
     public static final String PRODUCT_URL = "https://www.google.com";
     public static final String Properties = "webdriver.gecko.driver";
     public static final String get_url = "";
